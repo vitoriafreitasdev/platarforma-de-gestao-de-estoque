299f4a1157a0a6ea2b6090a1e45d5292d0d1759f
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-08-29 12:34:42 -0300

    base feita

--- frontend/src/routes/AdminRoute.tsx
+++ frontend/src/routes/AdminRoute.tsx
@@ -1,8 +1,40 @@
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState, useLayoutEffect } from "react"
+import { useParams } from "react-router-dom"
+import systemFetch from "../axios"
 
 const AdminRoute = () => {
+    const [admin, setAdmin] = useState<object | null>(null)
+    const [error, setError] = useState<string>("")
+    const {id} = useParams()
+
+
+
+    useLayoutEffect(() => {
+        const loadAdmin = async () => {
+            try {
+                const res = await systemFetch(`user/${id}`)
+
+                setAdmin(res.data)
+            } catch (error: any) {
+                setError(error.response.data.msg)
+                console.log(error)
+            }
+        }
+
+        loadAdmin()
+    }, [])
+
+    console.log(admin)
+
   return (
-    <div>AdminRoute</div>
+    <div>
+        {!error ? 
+        <div>
+            <p>rota do administrador</p>
+        </div> 
+        : <p>{error}</p>}        
+    </div>
   )
 }
 
